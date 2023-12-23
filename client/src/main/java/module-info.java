4cd95e8ed634f754a90fa873f76d77eb047ca7f9
jerric <jerricgao@gmail.com>
2023-12-23 13:57:52 -0700

    remove client

--- client/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module net.lliira.game.campaign.lliiragamecampaignclient {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens net.lliira.game.campaign.lliiragamecampaignclient to javafx.fxml;
-    exports net.lliira.game.campaign.lliiragamecampaignclient;
-}
